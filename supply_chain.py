653f9d9042d1e2df06e4fcea4a40e7eb50186105
susanlum <105427308+susanlum@users.noreply.github.com>
2023-03-04 17:28:57 +0800

    Update supply_chain.py

--- supply_chain.py
+++ supply_chain.py
@@ -134,9 +134,9 @@ X_test_scaled_df.describe()
 # Classification Models
 #-------------------------------------------------------------------------------------------------------------------------------------------#
 
-==============
+#==============#
 # (1) KNN 
-==============
+#==============#
 from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train_scaled_df, y_train)
@@ -188,9 +188,9 @@ print(confusion_matrix(y_test, y_pred))
 
 print(classification_report(y_test, y_pred))
 
-============================
+#============================#
 # (2) Logistics Regression
-============================
+#============================#
 # import the class
 from sklearn.linear_model import LogisticRegression
 
@@ -211,6 +211,89 @@ print(confusion_matrix(y_test, y_pred))
 
 print(classification_report(y_test, y_pred))
 
+from sklearn.metrics import roc_curve
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import roc_auc_score
+
+# define metrics
+y_pred_proba = logreg.predict_proba(X_test_scaled_df)[:,1]
+fpr1, tpr1, _ = roc_curve(y_test,  y_pred_proba)
+auc = roc_auc_score(y_test, y_pred_proba)
+
+# roc curve for tpr = fpr 
+random_probs = [0 for i in range(len(y_test))]
+p_fpr, p_tpr, _ = roc_curve(y_test, random_probs, pos_label=1) 
+
+# create ROC curve
+plt.plot(fpr1,tpr1,label="AUC="+str(auc))
+
+plt.plot(p_fpr, p_tpr, linestyle='--', color='blue')
+
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.legend(loc=4)
+
+plt.show()
+
+#============================#
+# (3) Support Vector Machine
+#============================#
+
+from sklearn.svm import SVC
+model = SVC()
+
+model.fit(X_train_scaled_df, y_train)
+
+model.score(X_test_scaled_df, y_test)
+
+#============================#
+# (4) Decision Tree
+#============================#
+# Import library
+from sklearn.tree import DecisionTreeClassifier # to build a classification tree
+from sklearn.tree import plot_tree # to draw a classification tree 
+
+# Create a decision tree and fit it to the training data
+clf_dt = DecisionTreeClassifier(random_state=42)
+clf_dt = clf_dt.fit(X_train_scaled_df, y_train)
+
+# plot decision tree
+plt.figure(figsize=(15,7.5))
+plot_tree(clf_dt,
+          filled=True,
+          rounded=True,
+          class_names=["On Time", "Not On Time"],
+          feature_names= X.columns);
+
+y_pred = clf_dt.predict(X_test_scaled_df)
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+print(confusion_matrix(y_test, y_pred))
+
+print(classification_report(y_test, y_pred))
+
+# plot_confusion_matrix() will run the test data down the tree and draw a confusion matrix. 
+plot_confusion_matrix(clf_dt, X_test_scaled_df, y_test, display_labels=["On Time", "Not On Time"])
+
+from sklearn.ensemble import BaggingClassifier
+
+bag_model = BaggingClassifier(
+    base_estimator=DecisionTreeClassifier(), 
+    n_estimators=5, 
+    max_samples=0.8, 
+    oob_score=True,
+    random_state=0
+)
+bag_model.fit(X_train_scaled_df, y_train)
+bag_model.oob_score_
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+scores = cross_val_score(RandomForestClassifier(n_estimators=5), X_train_scaled_df, y_train, cv=5)
+scores.mean()
 
 
 
