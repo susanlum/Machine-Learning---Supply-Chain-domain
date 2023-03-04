f11ec0d8e21e95c3d3a85f2f5f3f01557c36f473
susanlum <105427308+susanlum@users.noreply.github.com>
2023-03-04 16:55:53 +0800

    Update supply_chain.py

--- supply_chain.py
+++ supply_chain.py
@@ -45,4 +45,25 @@ sns.countplot(x = df['Customer_rating']);
 # To plot number of Reached On Time where 1 Indicates that the product has NOT reached on time and 0 indicates it has reached on time.
 sns.countplot(x = df['Reached.on.Time_Y.N']);
 
+# To plot Customer Rating versus Reached On Time
+sns.countplot(x='Customer_rating', data=df, hue='Reached.on.Time_Y.N')
+plt.show()
+
+# 2) Is Customer query is being answered?
+sns.countplot(x = df['Customer_care_calls']);
+
+# To plot Customer_care_calls versus Reached.on.Time_Y.N
+sns.countplot(x='Customer_care_calls', data=df, hue='Reached.on.Time_Y.N')
+plt.show()
+
+# 3) If Product importance is high. having highest rating or being delivered on time?
+# To plot Product Importance (Low, Medium, High) versus Customer Rating [from 1 (Worst) to 5 (Best)]
+sns.countplot(x='Product_importance', data=df, hue='Customer_rating')
+plt.show()
+
+# To plot Product Importance (Low, Medium, High) versus Reached.on.Time_Y.N (where 1=product has NOT reached on time; 0=product has reached on time)
+sns.countplot(x='Product_importance', data=df, hue='Reached.on.Time_Y.N')
+plt.show()
+
+# **Insights:** Based on the barplots above, high product importance does not affect customer rating. However, when product importance is high, the products have not reached on time.
 
