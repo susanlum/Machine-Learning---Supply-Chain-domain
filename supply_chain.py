af8c274dbc54e7ea498e2ff2dfddeff79a2c7b4c
susanlum <105427308+susanlum@users.noreply.github.com>
2023-03-04 16:46:39 +0800

    Update supply_chain.py

--- supply_chain.py
+++ supply_chain.py
@@ -1,1 +1,12 @@
-
+---------------------------------------------------------------------------------------------------------------------------------------------
+Load & Overview of Data
+---------------------------------------------------------------------------------------------------------------------------------------------
+# importing required libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import RobustScaler # for RobustScaler
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import confusion_matrix # to create a confusion matrix
+from sklearn.metrics import plot_confusion_matrix # to draw a confusion matrix
